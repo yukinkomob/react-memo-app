4419a100d69afd40f83189897cd996fb32e8db70
Nishimura <yukinkopen@gmail.com>
2021-09-17 23:17:09 +0900

    [mod] 削除処理を追加

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 
 interface Task {
+  id: string,
   title: string,
   category: string,
   description: string,
@@ -30,6 +31,7 @@ function List() {
         console.log(res);
         const taskList = res.data.map((d: any) => {
           const task = {
+            id: d.id,
             title: d.title,
             category: d.category,
             description: d.description,
@@ -46,6 +48,45 @@ function List() {
       });
   }
 
+  function deleteTask(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    console.log('e.currentTarget.id', e.currentTarget.id);
+    const id = e.currentTarget.id.split('delBtn')[1];
+
+    const token = localStorage.getItem('token');
+    console.log('token', token);
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    axios
+      .delete(`https://raisetech-memo-api.herokuapp.com/api/memo/${id}`, {
+        headers,
+      })
+      .then((res) => {
+        console.log(res);
+        const taskList = res.data.map((d: any) => {
+          const task = {
+            id: d.id,
+            title: d.title,
+            category: d.category,
+            description: d.description,
+            date: d.date,
+            markDiv: d.mark_div,
+          };
+          return task;
+        });
+        setTasks(taskList);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    const newTasks = tasks?.filter((t) => t.id !== id);
+    console.log('newTasks', newTasks);
+    setTasks(newTasks);
+  }
+
   useEffect(() => {
     getTasks();
   }, []);
@@ -53,7 +94,13 @@ function List() {
   return (
     <div>
       <ul>
-        {tasks && tasks.map((t: Task) => <li>{t.title}</li>)}
+        {tasks && tasks.map((t: Task) => (
+          <li>
+            {t.title}
+            {' '}
+            <button id={`delBtn${t.id}`} type="button" onClick={deleteTask}>削除</button>
+          </li>
+        ))}
       </ul>
       <button type="button" onClick={useHistory().goBack}>戻る</button>
     </div>
