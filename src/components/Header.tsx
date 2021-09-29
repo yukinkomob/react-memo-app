0d6143f4936f306e62fd765123213bdb7eddd3bf
Nishimura <yukinkopen@gmail.com>
2021-09-29 17:40:39 +0900

    [mod] ヘッダの配色を修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,65 +20,63 @@ const Header: React.FC<Props> = ({ onNewTask, onRecommendTask, onChangePage }) =
 
       <div className="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul className="navbar-nav me-auto mb-2 mb-md-0">
-          <li id="list-" className="nav-item nav-link list-unstyled link-primary cursor-pointer" onClick={onNewTask} aria-hidden>
+          <li id="list-" className="nav-item nav-link list-unstyled link-primary cursor-pointer link-purple-color" onClick={onNewTask} aria-hidden>
             <i className="far fa-sticky-note" />
             {' '}
             書く
           </li>
-          <li id="list-" className="nav-item nav-link list-unstyled link-primary cursor-pointer" onClick={() => onChangePage('list')} aria-hidden>
+          <li id="list-" className="nav-item nav-link list-unstyled link-primary cursor-pointer link-purple-color" onClick={() => onChangePage('list')} aria-hidden>
             <i className="fas fa-list" />
             {' '}
             並べて表示
           </li>
-          <li id="list-" className="nav-item nav-link list-unstyled link-primary cursor-pointer" onClick={onRecommendTask} aria-hidden>
+          <li id="list-" className="nav-item nav-link list-unstyled link-primary cursor-pointer link-purple-color" onClick={onRecommendTask} aria-hidden>
             <i className="fas fa-hand-holding-heart" />
             {' '}
             おまかせ表示
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="http://www.google.com/">
-              <i className="fas fa-paperclip" />
-              {' '}
-              振り返る
-            </a>
+          <li id="list-" className="nav-item nav-link list-unstyled link-primary cursor-pointer link-purple-color" aria-hidden>
+            <i className="fas fa-hand-holding-heart" />
+            {' '}
+            振り返る
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="https://suzuri.jp/Memooth24/">
+            <a className="nav-link link-purple-color" href="https://suzuri.jp/Memooth24/">
               <i className="fas fa-store" />
               {' '}
               ストア
             </a>
           </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="http://www.google.com/" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">
+          <li className="nav-item dropdown link-purple-color">
+            <a className="nav-link dropdown-toggle link-purple-color" href="http://www.google.com/" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">
               <i className="fas fa-user-cog" />
               {' '}
               設定
             </a>
             <ul className="dropdown-menu" aria-labelledby="dropdown01">
               <li>
-                <a className="dropdown-item" href="http://www.google.com/">
+                <a className="dropdown-item link-purple-color" href="http://www.google.com/">
                   <i className="fas fa-sign-out-alt" />
                   {' '}
                   ログアウト
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" href="http://www.google.com/">
+                <a className="dropdown-item link-purple-color" href="http://www.google.com/">
                   <i className="fas fa-cog" />
                   {' '}
                   アプリの設定
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" href="http://www.google.com/">
+                <a className="dropdown-item link-purple-color" href="http://www.google.com/">
                   <i className="fas fa-user" />
                   {' '}
                   アカウントの設定
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" href="http://www.google.com/">
+                <a className="dropdown-item link-purple-color" href="http://www.google.com/">
                   <i className="fab fa-android" />
                   {' '}
                   Android /
@@ -89,14 +87,14 @@ const Header: React.FC<Props> = ({ onNewTask, onRecommendTask, onChangePage }) =
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" href="http://www.google.com/">
+                <a className="dropdown-item link-purple-color" href="http://www.google.com/">
                   <i className="fas fa-arrow-circle-up" />
                   {' '}
                   有料版のご紹介
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" href="http://www.google.com/">
+                <a className="dropdown-item link-purple-color" href="http://www.google.com/">
                   <i className="fas fa-laptop" />
                   {' '}
                   このアプリについて
@@ -107,7 +105,7 @@ const Header: React.FC<Props> = ({ onNewTask, onRecommendTask, onChangePage }) =
         </ul>
         <form className="d-flex">
           <input className="form-control me-2" type="search" placeholder="検索" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
+          <button className="btn btn-outline-header" type="submit">Search</button>
         </form>
       </div>
     </div>
