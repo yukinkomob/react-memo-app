5cd1d61160bb3f71f8df7e8734d4b32e07183b79
Nishimura <yukinkopen@gmail.com>
2021-11-06 10:30:11 +0900

    [mod] おまかせ表示選択時の不要なAPIアクセスを除去

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -63,36 +63,42 @@ function Main() {
   function getRandomTasks() {
     console.log('getTasks');
 
-    const token = localStorage.getItem('token');
-    console.log('token', token);
-    const headers = {
-      Authorization: `Bearer ${token}`,
-    };
+    const total = Math.floor(1000000 * Math.random());
+    if (tasks) {
+      const base = total % tasks.length;
+      setNewId(tasks[base].id);
+    }
 
-    axios
-      .get('https://raisetech-memo-api.herokuapp.com/api/memos', {
-        headers,
-      })
-      .then((res) => {
-        console.log(res);
-        const taskList = res.data.map((d: any) => {
-          const task = {
-            id: d.id,
-            title: d.title,
-            category: d.category,
-            description: d.description,
-            date: d.date,
-            markDiv: d.mark_div,
-          };
-          return task;
-        });
-        const total = Math.floor(1000000 * Math.random());
-        const base = total % taskList.length;
-        setNewId(taskList[base].id);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    // const token = localStorage.getItem('token');
+    // console.log('token', token);
+    // const headers = {
+    //   Authorization: `Bearer ${token}`,
+    // };
+
+    // axios
+    //   .get('https://raisetech-memo-api.herokuapp.com/api/memos', {
+    //     headers,
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //     const taskList = res.data.map((d: any) => {
+    //       const task = {
+    //         id: d.id,
+    //         title: d.title,
+    //         category: d.category,
+    //         description: d.description,
+    //         date: d.date,
+    //         markDiv: d.mark_div,
+    //       };
+    //       return task;
+    //     });
+    //     const total = Math.floor(1000000 * Math.random());
+    //     const base = total % taskList.length;
+    //     setNewId(taskList[base].id);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
   }
 
   function recommendTask() {
